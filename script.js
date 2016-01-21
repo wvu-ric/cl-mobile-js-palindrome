6dc9d8e0f9e36795a04a3ea1c666d134a3aed772
AustinRClark <aoclark@mix.wvu.edu>
2016-01-21 12:49:25 -0500

    Completed Assignment - Palindrome.

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 var testWord = prompt("Enter a word");
 
+function reverseword(word) {
+	return word.split("").reverse().join("");
+}
+
 function isPalindrome(word){
 	// Returns true if palindrome
-	// Returns false otherwise	
+	thebird =  testWord.toString().replace(/\s+/g , '').toLowerCase();
+	theword = reverseword(thebird);
+	if (thebird == theword) {
+		return true;
+	}
+	// Returns false otherwise
+	else { return false;}
 }
 
 if(isPalindrome(testWord)){
-	alert("OH COOL A PALINDROME");
+	alert("OH COOL A PALINDROME. The bird is in fact the word.");
 }else{
-	alert(testWord + "is not a palindrome");
+	alert(testWord + " is not a palindrome. The bird is less than but not equal to the word.");
 }
