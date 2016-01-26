9d0876e417d4e5b52557f028a5ee16a38d792f40
stadelmanma <stadelmanma@gmail.com>
2016-01-26 16:29:55 -0500

    completed assignment2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var testWord = prompt("Enter a word");
 
 function isPalindrome(word){
+    word.split(' ').join('');
     word = word.toLowerCase();
 	rev_word = word.split('').reverse().join('');
 	return (word == rev_word)
