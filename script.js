db89ad41fe744cbdb773e21ebb0f7c9d65d0c568
jcordonier <cordonierjf@gmail.com>
2016-02-01 22:48:34 -0500

    palindrome complete

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 var testWord = prompt("Enter a word");
 
+function reverse(s) {
+  return s.split('').reverse().join('');
+}
+
 function isPalindrome(word){
 	// Returns true if palindrome
-	// Returns false otherwise	
+	// Returns false otherwise
+	var noSpaceNew  = testWord.replace(/\s/g, '');
+	var lowerCaseNew = noSpaceNew.toLowerCase();
+	var reverseWord = reverse(lowerCaseNew);
+
+	var testWordNew = testWord.replace(/\s/g, '');
+	var lowerCaseTest = testWordNew.toLowerCase();
+
+	if(lowerCaseTest == reverseWord){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 if(isPalindrome(testWord)){
 	alert("OH COOL A PALINDROME");
 }else{
-	alert(testWord + "is not a palindrome");
+	alert(testWord + " is not a palindrome");
 }
