2d87ce598ca58210aec99c85ecb3ee8f5db0565f
NicCronin <nicronin@mix.wvu.edu>
2016-02-02 13:21:19 -0500

    Palindrome Assignment

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 var testWord = prompt("Enter a word");
 
-function isPalindrome(word){
-	// Returns true if palindrome
-	// Returns false otherwise	
+function reverse(s){
+	return s.split("").reverse().join("");
 }
+
+function isPalindrome(word){
+
+	// Returns true if palindrome
+	if(reverse(word) == word){
+		return true;
+	}else{
+		// Returns false otherwise
+		return false;
+	}
+
 
 if(isPalindrome(testWord)){
 	alert("OH COOL A PALINDROME");
 }else{
-	alert(testWord + "is not a palindrome");
+	alert(testWord + " is not a palindrome");
 }
